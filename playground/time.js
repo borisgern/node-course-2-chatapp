0e263dee7b65f0fee07010d73df0878e2c1280b2
borisgern <borisgern@gmail.com>
2018-06-04 16:54:16 +0300

    Format timestamps using moments

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+// var date = moment();
+// date.add(1, 'years').subtract(1, 'month');
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+console.log(date.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -9,34 +9,39 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New meessage', message);
+  var formattedTime = moment(message.createdAt).format('H:mm');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('H:mm');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
+
   e.preventDefault();
 
   var messageTextbox = jQuery('[name=message]');
 
-  socket.emit('createMessage', {
-    from: 'User',
-    text: messageTextbox.val()
-  }, function () {
-    messageTextbox.val('')
-  });
+  if (messageTextbox.val()) {
+    socket.emit('createMessage', {
+      from: 'User',
+      text: messageTextbox.val()
+    }, function () {
+      messageTextbox.val('')
+    });
+  }
+  messageTextbox.focus();
 });
 
 var locationButton = jQuery('#send-location');
